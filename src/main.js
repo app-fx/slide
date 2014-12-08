417b403b4b2ad11613bbe5fdf8e76fbd5741bfad
Makis Tracend <makis.tracend@gmail.com>
2014-12-07 20:50:59 -0800

    main logic

--- src/main.js
+++ src/main.js
@@ -1,18 +1,17 @@
-/*
 (function() {
 	// Creates an object based in the HTML Element prototype
 	var el = Object.create(HTMLElement.prototype);
-	var view;
 
 	// Fires when an instance of the element is created
 	el.createdCallback = function() {
 
 		// gather options
 		var options = {};
+		if( this.attributes.state ) options.state = this.attributes.state.value;
 		// ...
 		options.el = this;
 		// instantiate view
-		view = new APP.UI.Component( options );
+		this.view = new APP.FX.Slide(options);
 
 	};
 
@@ -21,14 +20,31 @@
 
 	// Fires when an instance was removed from the document
 	el.detachedCallback = function() {
-		view.destroy();
+		this.view.destroy();
 	};
 
 	// Fires when an attribute was added, removed, or updated
-	el.attributeChangedCallback = function(attr, oldVal, newVal) {};
+	el.attributeChangedCallback = function(attr, oldVal, newVal) {
+		// redirect basted on attribute
+		switch( attr ){
+			case "state":
+				var state = false;
+				// filter state of interest
+				if( newVal.indexOf("slide-left") > -1 ) state = "slide-left";
+				if( newVal.indexOf("slide-right") > -1 ) state = "slide-right";
+				if( newVal.indexOf("slide-up") > -1 ) state = "slide-up";
+				if( newVal.indexOf("slide-down") > -1 ) state = "slide-down";
+				this.view.updateState( state );
+			break;
+			default:
+				// do nothing by default
+			break;
+		}
+	};
 
-	document.registerElement('ui-component', {
-		prototype: el
+	document.registerElement('fx-zoom', {
+		prototype: el,
+		extends: 'div'
 	});
+
 }());
-*/
